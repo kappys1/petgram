194dc77ca6b4f34a120688e166120cd1e809a1fd
Alex Marcos <alex.marcos_gutierrez@roche.com>
2021-09-08 23:31:54 +0200

    Detail page with reach/router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React from 'react'
 import { GlobalStyle } from './styles/GlobalStyles'
 import { Logo } from './components/Logo'
-import { PhotoCardWithQuery } from './container/PhotoCard'
 import { Home } from './pages/Home'
 import { Router } from '@reach/router'
+import { Detail } from './pages/Detail'
 
 export const App = () => {
-  const urlParams = new window.URLSearchParams(window.location.search)
-  const detailId = urlParams.get('detail')
   return (
     <div>
-      <Logo />
       <GlobalStyle />
-      {detailId
-        ? <PhotoCardWithQuery id={detailId} />
-        : (
-          <Router>
-            <Home path='/' />
-            <Home path='/pet/:categoryId' />
-          </Router>
-          )}
+      <Logo />
+      <Router>
+        <Home path='/' />
+        <Home path='/pet/:categoryId' />
+        <Detail path='/detail/:detailId' />
+      </Router>
     </div>
   )
 }
---
+++ src/pages/Detail.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import { PhotoCardWithQuery } from '../container/PhotoCard'
+
+export const Detail = ({ detailId }) => {
+  return <PhotoCardWithQuery id={detailId} />
+}

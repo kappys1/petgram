c1659541ea218545bda321188c90303b310c95a1
Alex Marcos <alex.marcos_gutierrez@roche.com>
2021-09-08 12:41:40 +0200

    show category bar fixed on scroll

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -5,6 +5,7 @@ import { List, Item } from './styles'
 
 export const ListOfCategories = () => {
   const [categories, setCategories] = useState([])
+  const [showFixed, setShowFixed] = useState(false)
 
   useEffect(function () {
     window.fetch('https://petgram-server-kappys-5p5zyiegt-kappys1.vercel.app/categories')
@@ -14,13 +15,29 @@ export const ListOfCategories = () => {
       })
   }, [])
 
-  return (
-    <List>
+  useEffect(function () {
+    const onScroll = e => {
+      const newShowFixed = window.scrollY > 200
+      showFixed !== newShowFixed && setShowFixed(newShowFixed)
+    }
+    document.addEventListener('scroll', onScroll)
+
+    return () => document.removeEventListener('scroll', onScroll)
+  }, [showFixed])
+
+  const renderList = (fixed) => (
+    <List className={fixed ? 'fixed' : ''}>
       {categories.map((category) => (
         <Item key={category.id}>
           <Category {...category} />
         </Item>
       ))}
     </List>
+  )
+  return (
+    <>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </>
   )
 }

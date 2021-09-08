0afba055e23b539710e597b2375da6cff75817e3
Alex Marcos <alex.marcos_gutierrez@roche.com>
2021-09-08 13:19:27 +0200

    load polyfill just when we need the polyfill

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,41 +1,47 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { Article, Button, Img, ImgWrapper } from './styles'
 import { MdFavoriteBorder } from 'react-icons/md'
-const DEFAULT_IMAGE = 'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png'
+const DEFAULT_IMAGE =
+  'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png'
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
   const ref = useRef(null)
   const [show, setShow] = useState(false)
-  useEffect(function () {
-    import('intersection-observer').then(() => {
-      const observer = new window.IntersectionObserver(function (entries) {
-        const { isIntersecting } = entries[0]
-        if (isIntersecting) {
-          console.log('si')
-          setShow(true)
-          observer.disconnect()
-        }
+  useEffect(
+    function () {
+      Promise.resolve(
+        typeof window.IntersectionObserver !== 'undefined'
+          ? window.IntersectionObserver
+          : import('intersection-observer')
+      ).then(() => {
+        const observer = new window.IntersectionObserver(function (entries) {
+          const { isIntersecting } = entries[0]
+          if (isIntersecting) {
+            console.log('si')
+            setShow(true)
+            observer.disconnect()
+          }
+        })
+        observer.observe(ref.current)
       })
-      observer.observe(ref.current)
-    })
-  }, [ref])
+    },
+    [ref]
+  )
 
   return (
     <Article ref={ref}>
-      {
-        show &&
-          <>
-            <a href={`/detail/${id}`}>
-              <ImgWrapper>
-                <Img src={src} />
-              </ImgWrapper>
-            </a>
-            <Button>
-              <MdFavoriteBorder size='32px' /> {likes} likes!
-            </Button>
-          </>
-      }
-
+      {show && (
+        <>
+          <a href={`/detail/${id}`}>
+            <ImgWrapper>
+              <Img src={src} />
+            </ImgWrapper>
+          </a>
+          <Button>
+            <MdFavoriteBorder size='32px' /> {likes} likes!
+          </Button>
+        </>
+      )}
     </Article>
   )
 }

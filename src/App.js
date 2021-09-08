05e48d956dbf53f70a187f55d98902164a4f3226
Alex Marcos <alex.marcos_gutierrez@roche.com>
2021-09-08 12:01:19 +0200

    add animations in Photocard

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { ListOfCategories } from './components/ListOfCategories'
-import { GlobalStyle } from './GlobalStyles'
+import { GlobalStyle } from './styles/GlobalStyles'
 import { ListOfPhotocards } from './components/ListOfPhotoCards'
+import { Logo } from './components/Logo'
 
 export const App = () => (
   <div>
+    <Logo />
     <GlobalStyle />
     <ListOfCategories />
     <ListOfPhotocards />
   </div>
-
 )

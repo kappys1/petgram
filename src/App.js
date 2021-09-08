a557d489cc6cc54622760c6601fa8ee156fdf26f
Alex Marcos <alex.marcos_gutierrez@roche.com>
2021-09-08 23:45:13 +0200

    add navbar

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Logo } from './components/Logo'
 import { Home } from './pages/Home'
 import { Router } from '@reach/router'
 import { Detail } from './pages/Detail'
+import { NavBar } from './components/NavBar'
 
 export const App = () => {
   return (
@@ -15,6 +16,7 @@ export const App = () => {
         <Home path='/pet/:categoryId' />
         <Detail path='/detail/:detailId' />
       </Router>
+      <NavBar />
     </div>
   )
 }

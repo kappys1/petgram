422019e685059878276e799ec8cbd9ab881e0075
Alex Marcos <alex.marcos_gutierrez@roche.com>
2021-09-10 11:44:35 +0200

    improve routes in our app

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react'
 import { GlobalStyle } from './styles/GlobalStyles'
 import { Logo } from './components/Logo'
 import { Home } from './pages/Home'
-import { Router } from '@reach/router'
+import { Redirect, Router } from '@reach/router'
 import { Detail } from './pages/Detail'
 import { NavBar } from './components/NavBar'
 import { Favs } from './pages/Favs'
 import { User } from './pages/User'
 import { NotRegisterUser } from './pages/NotRegisterUser'
 import { useStateValue } from './Context'
+import { NotFound } from './pages/NotFound'
 
 export const App = () => {
   const [{ isAuth }] = useStateValue()
@@ -17,24 +18,18 @@ export const App = () => {
       <GlobalStyle />
       <Logo />
       <Router>
+        <NotFound default />
         <Home path='/' />
         <Home path='/pet/:categoryId' />
         <Detail path='/detail/:detailId' />
+        {!isAuth && <NotRegisterUser path='/login' />}
+        {!isAuth && <Redirect noThrow from='/favs' to='/login' />}
+        {!isAuth && <Redirect noThrow from='/user' to='/login' />}
+        {isAuth && <Redirect noThrow from='/login' to='/' />}
+        <Favs path='/favs' />
+        <User path='/user' />
 
       </Router>
-      {
-      isAuth
-        ? (
-          <Router>
-            <Favs path='/favs' />
-            <User path='/user' />
-          </Router>)
-        : (
-          <Router>
-            <NotRegisterUser path='/favs' />
-            <NotRegisterUser path='/user' />
-          </Router>)
-        }
       <NavBar />
     </div>
   )

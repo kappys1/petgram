7c8b88fcb98c81a7161f5f5ff5aec382a3e191ef
Alex Marcos <alex.marcos_gutierrez@roche.com>
2021-09-13 12:53:06 +0200

    add manifest

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 // webpack.config.js
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-
+const WebpackPwaManifest = require('webpack-pwa-manifest')
+const path = require('path')
 const webpack = require('webpack')
+
 module.exports = {
   mode: 'development',
   output: {
@@ -14,6 +16,19 @@ module.exports = {
     }),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('development')
+    }),
+    new WebpackPwaManifest({
+      name: 'PetGram tu app de fotos de mascotas',
+      short_name: 'Petgram 🐶',
+      description: 'Pegram, fotos de animales domesticos',
+      background_color: '#fff',
+      theme_color: '#b1a',
+      icons: [
+        {
+          src: path.resolve('src/assets/icon.png'),
+          sizes: [96, 128, 192, 256, 384, 512]
+        }
+      ]
     })
   ],
   module: {

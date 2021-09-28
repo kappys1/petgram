b98c0e064b48a2a3bb614b7818e17547ce2054b9
Alex Marcos <alex.marcos_gutierrez@roche.com>
2021-09-28 19:24:08 +0200

    remove adapter

--- api/models/userModel.js
+++ api/models/userModel.js
@@ -1,4 +1,3 @@
-const db = require('../adapter')
 const crypto = require('crypto')
 const bcrypt = require('bcrypt')
 const { getElementCollection, editElementCollection, getElementCollectionByQuery, createElementIntoCollection, getAllElementsCollection } = require('../utils')

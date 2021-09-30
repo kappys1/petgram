c76d622c070bdfe4b41d0dd6cf445a54b785ab3b
Alex Marcos <alex.marcos_gutierrez@roche.com>
2021-09-30 18:12:37 +0200

    fix cover

--- src/components/PhotoCard/styles.js
+++ src/components/PhotoCard/styles.js
@@ -5,7 +5,7 @@ export const Img = styled.img`
   ${fadeIn({ time: '1s' })}
   box-shadow: 0 10px 14px rgba(0, 0, 0, 0.2);
   height: 100%;
-  object-fit: contain;
+  object-fit: cover;
   position: absolute;
   top: 0;
   width: 100%;
